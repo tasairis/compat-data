73d084f486f04dc7ecef10a1534535edcc2ead0e
tasairis <37535621+tasairis@users.noreply.github.com>
2023-09-24 23:12:52 -0700

    2023-09-24

--- docs/entry.js
+++ docs/entry.js
@@ -1,7 +1,7 @@
 
 (function() {
 	const {jsonp, progressp} = document.currentScript.dataset;
-	let mcount = 0, mods = 6579, scount = 0, sources = 19;
-	const ps = (["2013-3641101577","2014-3163946395","2015-3706573401","2021-1198791314","2017-962631614","2016-2187986467","2018-1014399542","2019-479868188","202302-2469522112","2022-2149047795","2020-2990600187","202306-1173075071","202307-3916613927","recent-304344223","latest-500300455","202304-2566541912","202301-898355340","202305-467598498","202303-812601498"]).map(k => import(`./${k}.js`).then(m => [m.default, progressp && window[progressp].call(undefined, mcount += m.default.length, mods, ++scount, sources)][0]));
-	Promise.all(ps).then(ds => ({ date: "2023-09-11", skyrim: "1.6.640", data: ds.flat(), sources })).then(d => window[jsonp](d));
+	let mcount = 0, mods = 6580, scount = 0, sources = 19;
+	const ps = (["2013-3641101577","2014-2145674304","2015-3328205207","2021-1777149587","2017-2292702372","2016-3214249736","2018-570172706","2019-479868188","202302-2469522112","2022-4087479863","2020-1071706098","202306-582642981","202307-3410325262","latest-227044691","recent-1041785478","202304-2869106261","202301-898355340","202305-4280720953","202303-812601498"]).map(k => import(`./${k}.js`).then(m => [m.default, progressp && window[progressp].call(undefined, mcount += m.default.length, mods, ++scount, sources)][0]));
+	Promise.all(ps).then(ds => ({ date: "2023-09-25", skyrim: "1.6.640", data: ds.flat(), sources })).then(d => window[jsonp](d));
 })();
