83e677f684763f89e690af4b2678c60032624a1e
tasairis <37535621+tasairis@users.noreply.github.com>
2024-03-14 09:08:42 -0700

    2024-03-14

--- docs/entry.js
+++ docs/entry.js
@@ -1,9 +1,9 @@
 
 (function() {
 	const { jsonp, progressp } = document.currentScript.dataset;
-	const info = ({"date":"2024-03-11","skyrim":"1.6.1170"});
-	let mcount = 0, mods = 7045, scount = 0, sources = 14;
-	const ps = (["2023-4133681089","2021-1032610986","2020-2282780285","2018-3872997318","2022-857951154","2016-2501429297","2014-1729027668","2017-1194892118","2015-3017351910","2019-3016863612","2013-2889172304","2024-02-2111685696","2024-01-1332338645","recent-3977074618"]).map(key =>
+	const info = ({"date":"2024-03-14","skyrim":"1.6.1170"});
+	let mcount = 0, mods = 7052, scount = 0, sources = 14;
+	const ps = (["2023-4222232226","2021-1032610986","2020-2282780285","2018-3872997318","2022-1799031038","2016-2501429297","2014-1729027668","2017-1194892118","2015-3017351910","2019-3016863612","2013-2889172304","2024-02-2854628909","2024-01-1719873113","recent-2421200571"]).map(key =>
 		import(`./${key}.js`).then(source =>
 			(progressp && window[progressp]?.call(undefined, key, null, mcount += source.default.length, mods, ++scount, sources, source.default), source.default)
 		).catch(error =>
