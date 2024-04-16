36a126b50e77efb898682aebeb466f3beb754ded
tasairis <37535621+tasairis@users.noreply.github.com>
2024-04-16 14:25:24 -0700

    2024-04-16

--- docs/entry.js
+++ docs/entry.js
@@ -1,9 +1,9 @@
 
 (function() {
 	const { jsonp, progressp } = document.currentScript.dataset;
-	const info = ({"date":"2024-04-02","skyrim":"1.6.1170"});
-	let mcount = 0, mods = 7108, scount = 0, sources = 15;
-	const ps = (["2023-2359510318","2021-2257353200","2020-2282780285","2018-1206393470","2022-3576218491","2016-3089888370","2014-580305339","2017-3243555216","2015-4154056527","2019-51867596","2013-1418670436","2024-02-1189169730","2024-01-1753966181","2024-03-353453677","recent-2482665663"]).map(key =>
+	const info = ({"date":"2024-04-16","skyrim":"1.6.1170"});
+	let mcount = 0, mods = 7126, scount = 0, sources = 15;
+	const ps = (["2023-4290285608","2021-305004688","2020-1180757651","2018-1206393470","2022-3034702921","2016-2885214449","2014-3384309543","2017-271217873","2015-2613401117","2019-51867596","2013-2802801456","2024-02-3549854768","2024-01-1753966181","recent-2756445655","2024-03-655246191"]).map(key =>
 		import(`./${key}.js`).then(source =>
 			(progressp && window[progressp]?.call(undefined, key, null, mcount += source.default.length, mods, ++scount, sources, source.default), source.default)
 		).catch(error =>
